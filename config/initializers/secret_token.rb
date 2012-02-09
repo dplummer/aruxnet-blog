63f60215868ebd63f50f1bfe165983f198a75265
Donald Plummer <donald@cideasphere.com>
2012-02-08 16:07:50 -0800

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AruxnetBlog::Application.config.secret_token = 'baf11dc6a815c15adca89ec9b17a7cdaa3cf864a62815284296006d868374be3c84113309ef81dd81dab19df2d826cb53713336ccf36202f1db283480736a59e'
